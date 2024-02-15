cc3fa3e7d05c489d8964ddd13dd34d9302a02c02
Smith_Lai <smith_lai@usiglobal.com>
2024-02-15 10:38:53 +0800

    Add ollama path

--- docker/launch-jupyterlab.sh
+++ docker/launch-jupyterlab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . /app/venv/bin/activate
 ollama serve &
+# ollama pull llama2-uncensored
 #jupyter-lab --allow-root --ip 0.0.0.0  --ContentsManager.allow_hidden=True
 echo "allow 10 sec for JupyterLab to start @ http://$(hostname -I | cut -d' ' -f1):$JUPYTER_PORT " && \
 echo "JupterLab logging location:  /var/log/jupyter.log  (inside the container)" && \
